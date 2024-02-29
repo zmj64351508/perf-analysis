f1b4c60e6a2a023e11807577e51b5d7b7c4c0cc3
mingjun zhou <mingjun.zhou@guasemi.com>
2024-02-29 11:44:50 +0800

    add interaction
    mouse drag to move;
    mouse scroll to scale(xaxis without ctrl & yaxis with ctrl)

--- main.py
+++ main.py
@@ -3,6 +3,100 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from fnmatch import fnmatch
 
+class PlotInteraction(object):
+    def __init__(self, fig):
+        self.lastx = 0
+        self.lasty = 0
+        self.press = False
+        self.ctrl = False
+        self.fig = fig
+        self.fig.canvas.mpl_connect("button_press_event", self.on_mouse_press(self))
+        self.fig.canvas.mpl_connect("button_release_event", self.on_mouse_release(self))
+        self.fig.canvas.mpl_connect("motion_notify_event", self.on_mouse_move(self))
+        self.fig.canvas.mpl_connect("key_press_event", self.on_key_press(self))
+        self.fig.canvas.mpl_connect("key_release_event", self.on_key_release(self))
+        self.fig.canvas.mpl_connect("scroll_event", self.on_mouse_scroll(self))
+
+    class on_key_press(object):
+        def __init__(self, holder):
+            self.holder = holder
+
+        def __call__(self, event):
+            if event.key == "control":
+                self.holder.ctrl = True
+
+    class on_key_release(object):
+        def __init__(self, holder):
+            self.holder = holder
+
+        def __call__(self, event):
+            if event.key == "control":
+                self.holder.ctrl = False
+
+    class on_mouse_press(object):
+        def __init__(self, holder):
+            self.holder = holder
+        def __call__(self, event):
+            if event.inaxes:  # 判断鼠标是否在axes内
+                if event.button == 1:  # 判断按下的是否为鼠标左键1（右键是3）
+                    self.holder.press = True
+                    self.holder.lastx = event.xdata  # 获取鼠标按下时的坐标X
+                    self.holder.lasty = event.ydata  # 获取鼠标按下时的坐标Y
+
+    class on_mouse_move(object):
+        def __init__(self, holder):
+            self.holder = holder
+        def __call__(self, event):
+            axtemp = event.inaxes
+            if axtemp:
+                if self.holder.press:  # 按下状态
+                    # 计算新的坐标原点并移动
+                    # 获取当前最新鼠标坐标与按下时坐标的差值
+                    x = event.xdata - self.holder.lastx
+                    y = event.ydata - self.holder.lasty
+                    # 获取当前原点和最大点的4个位置
+                    x_min, x_max = axtemp.get_xlim()
+                    y_min, y_max = axtemp.get_ylim()
+
+                    x_min = x_min - x
+                    x_max = x_max - x
+                    y_min = y_min - y
+                    y_max = y_max - y
+
+                    axtemp.set_xlim(x_min, x_max)
+                    axtemp.set_ylim(y_min, y_max)
+                    self.holder.fig.canvas.draw()  # 绘图动作实时反映在图像上
+
+    class on_mouse_release(object):
+        def __init__(self, holder):
+            self.holder = holder
+        def __call__(self, event):
+            if self.holder.press:
+                self.holder.press = False  # 鼠标松开，结束移动
+
+    class on_mouse_scroll(object):
+        def __init__(self, holder):
+            self.holder = holder
+
+        def __call__(self, event):
+            ax = event.inaxes
+            x_min, x_max = ax.get_xlim()
+            y_min, y_max = ax.get_ylim()
+            x_step = (x_max - x_min) / 10
+            y_step = (y_max - y_min) / 10
+            if event.button == 'up':
+                if not self.holder.ctrl:
+                    ax.set(xlim=(x_min + x_step, x_max - x_step))
+                else:
+                    ax.set(ylim=(y_min + y_step, y_max - y_step))
+            elif event.button == 'down':
+                if not self.holder.ctrl:
+                    ax.set(xlim=(x_min - x_step, x_max + x_step))
+                else:
+                    ax.set(ylim=(y_min - y_step, y_max + y_step))
+            self.holder.fig.canvas.draw_idle()
+
+
 def plot_record(records, start, end, type):
     x = range(0, records.get_total_ms(), records.get_interval_ms())
     fig = plt.figure()
@@ -19,6 +113,7 @@ def plot_record(records, start, end, type):
     cnt_plt.axvline(x=end, linestyle='--')
     fig.set_figheight(8)
     fig.set_figwidth(15)
+    PlotInteraction(fig)
 
 
 def plot_percent_sum(records, start, end, type, deno, nume):
@@ -196,5 +291,6 @@ if __name__ == '__main__':
     #plot_record(record_list, start, end, 'cnt')
     plot_record(record_list, start, end, 'access*bw')
     plot_record(record_list, start, end, 'cpu-cycles*bw')
+    #mplcursors.cursor()
     plt.show()
     
