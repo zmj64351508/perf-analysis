0944b75d0bd0f4bbb3d7dae10300fded9c684a70
mingjun zhou <mingjun.zhou@guasemi.com>
2024-12-13 19:29:20 +0800

    highlight data point nearest to mouse position

--- time_series_viewer.py
+++ time_series_viewer.py
@@ -63,23 +63,29 @@ class MarkCommand:
 
 
 class TimeSeriesViewerBase(tk.Toplevel):
-	def __init__(self, parent, fig):
+	def __init__(self, parent):
 		super().__init__(parent)
-		self._fig = fig
+		self.fig, self.ax = plt.subplots(figsize=(15, 7))
 		self.mark_command = None
 		self.mark_command_history = []
-		self.canvas = FigureCanvasTkAgg(fig, master=self)
+		self.canvas = FigureCanvasTkAgg(self.fig, master=self)
 		self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 		self.canvas.draw()
 		toolbar = NavigationToolbar2Tk(self.canvas, self)
 		toolbar.update()
 		self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 		self.canvas.mpl_connect("button_press_event", self.on_right_click)
+		self.canvas.mpl_connect('motion_notify_event', self.on_motion)
 		self.popup_menu = tk.Menu(self, tearoff=0)
+		self.popup_menu.add_command(label="Sync scale", command=self.menu_sync_scale)
 		self.popup_menu.add_command(label="Mark", command=self.menu_mark)
 		self.popup_menu.add_command(label="Sync marks", command=self.menu_sync_marks)
 		self.popup_menu.add_command(label="Undo mark", command=self.menu_undo_mark)
 		self.popup_menu.add_command(label="Clear mark", command=self.menu_clear_mark)
+		highlight_marker = '.'
+		if config['plot.marker'] == '.':
+			highlight_marker = 'o'
+		self.highlight, = self.ax.plot([], [], highlight_marker)
 		#self.window.protocol("WM_DELETE_WINDOW", self.on_close)
 
 	def set_window_title(self, title):
@@ -91,16 +97,66 @@ class TimeSeriesViewerBase(tk.Toplevel):
 		self.mark_command_history.append(command)
 		self.canvas.draw()
 
+	def on_motion(self, event):
+		ax = event.inaxes
+		if not ax:
+			return
+		self.nearest_point(ax, event.xdata, event.ydata)
+
 	def on_right_click(self, event):
+		ax = event.inaxes
+		if not ax:
+			return
 		if event.button == 3:
-			ax = event.inaxes
-			if ax:
-				try:
-					self.mark_x = event.xdata
-					self.popup_menu.tk_popup(event.guiEvent.x_root, event.guiEvent.y_root)
-					#event.guiEvent.handled = True
-				finally:
-					self.popup_menu.grab_release()
+			try:
+				self.mark_x = event.xdata
+				self.scale_x_min = ax.get_xlim()[0]
+				self.scale_x_max = ax.get_xlim()[1]
+				self.popup_menu.tk_popup(event.guiEvent.x_root, event.guiEvent.y_root)
+			finally:
+				self.popup_menu.grab_release()
+
+	def nearest_point(self, ax, x, y):
+		lines = self.get_lines()
+		series_count = len(lines)
+		if series_count == 0:
+			return
+		
+		xlim = ax.get_xlim()
+		xrange = xlim[1] - xlim[0]
+		ylim = ax.get_ylim()
+		yrange = ylim[1] - ylim[0]
+		min_index = np.zeros(series_count, dtype=int)
+		distances = np.zeros(series_count)
+		for i in range(series_count):
+			x_series = self.get_x_series(ax, i)
+			y_series = self.get_y_series(ax, i)
+			if x_series is None or y_series is None:
+				return
+			d = np.sqrt(((x_series - x) / xrange) ** 2 + ((y_series - y) / yrange) ** 2)
+			min_index[i] = np.argmin(d)
+			distances[i] = np.min(d)
+		series_index = np.argmin(distances)
+		value_index = min_index[series_index]
+		x_series = self.get_x_series(ax, series_index)
+		y_series = self.get_y_series(ax, series_index)
+		self.highlight.set_data(x_series[value_index], y_series[value_index])
+		self.highlight.set_color(lines[series_index].get_color())
+		self.fig.canvas.draw_idle()
+
+	def get_lines(self):
+		return []
+
+	def get_x_series(self, axes, index):
+		return None
+		
+	def get_y_series(self, axes, index):
+		return None
+
+	def set_x_scale(self, min, max):
+		for ax in self._fig.axes:
+			ax.set_xlim([min, max])
+		self.canvas.draw()
 
 	def undo_mark(self):
 		try:
@@ -116,6 +172,15 @@ class TimeSeriesViewerBase(tk.Toplevel):
 			command.undo_mark()
 		self.mark_command_history.clear()
 		self.canvas.draw()
+
+	def menu_sync_scale(self):
+		for k, v in viewer.items():
+			if v is not self:
+				v.set_x_scale(self.scale_x_min, self.scale_x_max)
+		for k, v in combined_all_series.items():
+			if v is not self:
+				v.set_x_scale(self.scale_x_min, self.scale_x_max)
+
 
 	def menu_mark(self):
 		if self.mark_x is not None:
@@ -170,8 +235,7 @@ class PerodicAnalysisViewer(TimeSeriesViewerBase):
 	def __init__(self, parent, name, series):
 		self.name = name
 		self.series = series
-		self.fig, self.ax = plt.subplots(figsize=(15, 7))
-		super().__init__(parent, self.fig)
+		super().__init__(parent)
 
 		self.set_window_title(self.name)
 		self.fig.canvas.mpl_connect('close_event', self.on_close)
@@ -184,7 +248,10 @@ class PerodicAnalysisViewer(TimeSeriesViewerBase):
 
 		frequency, power = LombScargle(time, values).autopower()
 
-		self.ax.plot(1 / frequency, power, marker=config["plot.marker"])
+
+		self.x = 1 / frequency
+		self.y = power
+		self.line = self.ax.plot(self.x, self.y, marker=config["plot.marker"])
 		self.ax.set_title(f'{name} Lomb-Scargle periodogram')
 		self.ax.set_xlabel('period')
 		self.ax.set_ylabel('power')
@@ -193,28 +260,48 @@ class PerodicAnalysisViewer(TimeSeriesViewerBase):
 		global perodic_analysis_viewers
 		perodic_analysis_viewers.pop(self.name, None)
 
+	def get_lines(self):
+		return [self.line]
+
+	def get_x_series(self, axes, index):
+		return self.x
+
+	def get_y_series(self, axes, index):
+		return self.y
+
 		
 class TimeSeriesCombinedViewer(TimeSeriesViewerBase):
 	def __init__(self, parent, all_series):
-		self.fig, self.ax = plt.subplots(figsize=(15, 7))
-		super().__init__(parent, self.fig)
+		super().__init__(parent)
 
 		self.time_unit = "ns"
 		self.unit = ""
 		self.lines = []
+		self.x = []
+		self.y = []
 		for name, series in all_series.items():
 			print(f"plotting {name}")
 			data = series.get_data_series()
 			timestamps = series.get_timestamp_series() if series.get_timestamp_series() is not None else np.arange(len(series.data))
-			self.ax.plot(timestamps, data, label=f"{name} ({series.get_unit()})", marker=config["plot.marker"])
+			self.lines += self.ax.plot(timestamps, data, label=f"{name} ({series.get_unit()})", marker=config["plot.marker"])
+			self.x.append(timestamps)
+			self.y.append(data)
 			if self.unit == "":
 				self.unit = series.get_unit()
 			elif self.unit != series.get_unit():
 				raise ValueError(f"All series must have the same unit. {self.unit} vs. {series.get_unit()}")
-
 		self.ax.legend(loc='upper left')
 		self.ax.set_xlabel(f"time ({self.time_unit})")
 		self.ax.set_ylabel(f"({self.unit})")
+
+	def get_lines(self):
+		return self.lines
+
+	def get_x_series(self, axes, index):
+		return self.x[index]
+
+	def get_y_series(self, axes, index):
+		return self.y[index]
 
 
 class TimeSeriesViewer(TimeSeriesViewerBase):
@@ -234,8 +321,7 @@ class TimeSeriesViewer(TimeSeriesViewerBase):
 			raise ValueError(f"Time and data series must have the same length. {len(self.timestamps)} vs. {len(series.data)}")
 
 		self.name = name
-		self.fig, self.ax = plt.subplots(figsize=(15, 7))
-		super().__init__(parent, self.fig)
+		super().__init__(parent)
 
 		self.set_window_title(self.name)
 		self.fig.canvas.mpl_connect('close_event', self.on_close)
@@ -252,6 +338,15 @@ class TimeSeriesViewer(TimeSeriesViewerBase):
 		self.span = SpanSelector(self.ax, self.on_select, 'horizontal', useblit=True,
 									props=dict(alpha=0.3, facecolor='blue'), interactive=True, button=1)
 		self.show_statistics(0, 0)
+
+	def get_lines(self):
+		return [self.line]
+
+	def get_x_series(self, axes, index):
+		return self.timestamps
+
+	def get_y_series(self, axes, index):
+		return self.data
 
 	def on_select(self, xmin, xmax):
 		self.show_statistics(xmin, xmax)
